899318129a69b3a863c87026f0a001429e031e10
M Sneijders <mj.sneijders@gmail.com>
2020-05-01 21:30:28 +0200

    unavailable value for heading now handled

--- VDM_msg.cpp
+++ VDM_msg.cpp
@@ -335,7 +335,12 @@ void tVdmMsg::Mesg1 (
               //SOG -> SOG_conv
                   uint16_t SOG_conv=(uint16_t)round(msToKnots(SOG)/0.1);
               //Heading -> Heading_conv
-                  uint16_t Heading_conv=(uint16_t)round(RadToDeg(Heading));
+				uint16_t Heading_conv;
+				if (Heading == -1e9){
+						Heading_conv=511;
+				}else{
+						Heading_conv=(uint16_t)round(RadToDeg(Heading));
+				}
               //ROT -> ROT_conv
                   int16_t ROT_conv=(int16_t)round(ROT/3.125E-05);
 	
@@ -488,7 +493,12 @@ void tVdmMsg::Mesg18 (
 		//SOG -> SOG_conv
 		  uint16_t SOG_conv=(uint16_t)round(msToKnots(SOG)/0.1);
 		//Heading -> Heading_conv
-		  uint16_t Heading_conv=(uint16_t)round(RadToDeg(Heading));			
+				uint16_t Heading_conv;
+				if (Heading == -1e9){
+						Heading_conv=511;
+				}else{
+						Heading_conv=(uint16_t)round(RadToDeg(Heading));
+				}			
 		Add4UByte( (uint32_t)0x00, 20); 
 		Add1UByte( (uint8_t)RAIM, 1); 
 		Add1UByte( Mode, 1); 
